0e2161f20038a08ced47d4327d65f2e2483a17af
Divya Kumari <meb25063@tezu.ac.in>
2025-10-22 14:17:32 +0530

    Add files via upload
    1. intro to C
    2. my introduction

---
+++ Lab_1/my_introduction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	printf("Name:Divya Kumari\n");
+	printf("Roll no.:MEB25063\n");
+	printf("Department:Mechanical\n");
+	printf("University:Tezpur university");
+	
+	
+	
+	
+	
+	
+	return 0;
+}
